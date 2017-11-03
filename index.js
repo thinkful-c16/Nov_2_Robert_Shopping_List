5288aa8606d55b7cd7cb587033d868727ceb2495
Robert Frazier <robert.v.frazier@gmail.com>
2017-11-03 00:26:17 -0400

    add item button now working perfectly

--- index.js
+++ index.js
@@ -2,8 +2,28 @@
 function addItem() {
   $('#js-shopping-list-form').submit(event => {
     event.preventDefault();
-    console.log('made it here?');
+    let newItem = $(this).find('input[name="shopping-list-entry"]').val();
+    $(this).find('input[name="shopping-list-entry"]').val('');
+    $("ul").append(`<li><span class="shopping-item">${newItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button>  <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
   });
 } 
 
+function toggleItem() {
+  $('.shopping-item-toggle').click(function(event) {
+    <!-- event.stopPropagation(); -->
+    console.log('word');
+  });
+}
+
+function deleteItem() {
+  $('.shopping-item-delete').click(function(event) {
+    event.stopPropagation();
+    $(this).remove();
+  });
+}
+
 $(addItem);
+$(toggleItem);
+$(deleteItem);
+
+<!-- shopping-list, li, shopping-item-controls, --> 
